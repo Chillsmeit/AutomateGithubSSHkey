814c55b6d603aa0098f81aaf1140ee69fdfe5ebd
Bernardo Melo <chillsmeit@proton.me>
2024-04-03 09:17:21 +0000

    Changed "echo -e" to "printf" to make the script show the color correctly in MacOS

--- CreateSSH-GithubKey.sh
+++ CreateSSH-GithubKey.sh
@@ -11,7 +11,7 @@ resetcolor='\e[0m'
 # Check sudo
 if [[ $EUID -eq 0 ]]; then
 	messagecolor=$red
-	echo -e "${red}Please do not run this script as sudo!"
+	printf "${red}Please do not run this script as sudo!\n"
 	read -r
 	exit 1
 fi
@@ -39,9 +39,10 @@ function answer_no() {
 			;;
 	esac
 }
-echo -e "${orange}####################"
-echo -e "${orange}# Checking system! #"
-echo -e "${orange}####################\n"
+
+printf "${orange}#######################################\n"
+printf "${orange}##          Checking system!         ##\n"
+printf "${orange}#######################################\n"
 
 # What is your OS?
 OS=$(uname)
@@ -49,20 +50,20 @@ userOS=""
 
 if [ "$OS" == "Darwin" ]; then
     userOS="MacOS"
-    echo -e "${resetcolor}Ok, I see you're using ${orange}$userOS!${resetcolor}"
+    printf "${resetcolor}Ok, I see you're using ${orange}$userOS!${resetcolor}\n"
 elif [ "$OS" == "Linux" ]; then
     userOS="Linux"
-    echo -e "${resetcolor}Ok, I see you're using ${orange}$userOS!${resetcolor}"
+    printf "${resetcolor}Ok, I see you're using ${orange}$userOS!${resetcolor}\n"
 else
-    echo -e "${red}Unsupported operating system: $OS${resetcolor}"
+    printf "${red}Unsupported operating system: $OS${resetcolor}\n"
     exit 1
 fi
 
 # Does ssh-keygen exist?
 if command -v ssh-keygen &> /dev/null; then
-    echo -e "${orange}ssh-keygen${resetcolor} package is ${green}available\n${resetcolor}"
+    printf "${orange}ssh-keygen${resetcolor} package is ${green}available!\n${resetcolor}"
 else
-    echo -e "${orange}ssh-keygen ${red}not found! ${resetcolor}Please install ${orange}OpenSSH${resetcolor} or make sure it's in your ${orange}PATH\n${resetcolor}Press Enter to exit"
+    printf "${orange}ssh-keygen ${red}not found! ${resetcolor}Please install ${orange}OpenSSH${resetcolor} or make sure it's in your ${orange}PATH\n${resetcolor}Press Enter to exit\n"
     read -r
     exit 1
 fi
@@ -70,27 +71,27 @@ fi
 # Create SSH path
 ssh_folder="$HOME/.ssh"
 if [ ! -d "$ssh_folder" ]; then
-    echo -e "${resetcolor}Creating ${orange}$ssh_folder\n${resetcolor}"
+    printf "${resetcolor}Creating ${orange}$ssh_folder${resetcolor}\n"
     mkdir -p "$ssh_folder"
 fi
 
-keyname="git$userOS" # get keyname based on the users OS
+keyname="github$userOS" # get keyname based on the users OS
 # Check if key already exists
 if [ -e "$ssh_folder/$keyname" ]; then
     while true; do
 
-	echo -e "${red}The key already exists!${resetcolor} Do you want to overwrite it?"
-	read -p "Enter your choice (y/n): " answer_overwrite_key
+	printf "${red}The key already exists!${resetcolor}\nDo you want to overwrite it?"
+	read -p " Enter your choice (y/n): " answer_overwrite_key
 	
 	if answer_yes "$answer_overwrite_key"; then
-	    echo -e "${resetcolor}\nOverwritting ${orange}keypair...\n${resetcolor}Press enter twice and leave the ${orange}passphrase empty${resetcolor} for now\n"
+	    printf "${resetcolor}\nOverwritting ${orange}keypair...\n${resetcolor}Press enter twice and leave the ${orange}passphrase empty${resetcolor} for now\n\n"
 	    sleep 1
 	    rm "$ssh_folder/$keyname" && rm "$ssh_folder/$keyname.pub"
 	    ssh-keygen -t rsa -f $ssh_folder/$keyname
-	    echo -e "\n${resetcolor}ssh keypair ${orange}$keyname${resetcolor} and ${orange}$keyname.pub${resetcolor} have been ${green}overwritten${resetcolor}"
+	    printf "\n${resetcolor}ssh keypair ${orange}$keyname${resetcolor} and ${orange}$keyname.pub${resetcolor} have been ${green}overwritten!${resetcolor}"
 	    break
 	elif answer_no "$answer_overwrite_key"; then
-	    echo -e "\n${red}Exiting..."
+	    printf "\n${red}Exiting..."
 	    exit 1
 	else
 	    echo ""
@@ -99,7 +100,7 @@ if [ -e "$ssh_folder/$keyname" ]; then
     # Create the key if it doesn't exist
 else
     ssh-keygen -t rsa -f $ssh_folder/$keyname
-    echo -e "\n${resetcolor}ssh keypair ${orange}$keyname${resetcolor} and ${orange}$keyname.pub${resetcolor} have been ${green}created${resetcolor}"
+    printf "\n${resetcolor}ssh keypair ${orange}$keyname${resetcolor} and ${orange}$keyname.pub${resetcolor} have been ${green}created${resetcolor}"
 fi
  
 # Create or overwrite SSH config file
@@ -108,8 +109,8 @@ if [ -e "$config_file" ]; then
     
     while true; do
     
-	echo -e "\n${red}The config file already exists! ${resetcolor}Do you want to overwrite it?"
-	read -p "Enter your choice (y/n): " answer_overwrite
+	printf "\n${red}The config file already exists!\n${resetcolor}Do you want to overwrite it?"
+	read -p " Enter your choice (y/n): " answer_overwrite
 
 	if answer_yes "$answer_overwrite"; then
 	    cat << EOF > "$config_file"
@@ -120,10 +121,10 @@ Host github.com
 	IdentityFile ~/.ssh/$keyname
 	IdentitiesOnly yes
 EOF
-	    echo -e "\n${green}Config file overwritten!${resetcolor}"
+	    printf "\n${green}Config file overwritten!${resetcolor}"
 	    break
 	elif answer_no "$answer_overwrite"; then
-	    echo -e "\n${red}Exiting..."
+	    printf "\n${red}Exiting..."
 	    exit 1
 	else
 	    echo ""
@@ -138,23 +139,23 @@ Host github.com
 	IdentityFile ~/.ssh/$keyname
 	IdentitiesOnly yes
 EOF
-    echo -e "\n${green}Config file created!${resetcolor}"
+    printf "\n${green}Config file created!${resetcolor}"
 fi
 
 # Copy the public key to clipboard
 if [ "$userOS" == "MacOS" ]; then
     pbcopy < "$ssh_folder/$keyname.pub"
-    echo -e "${green}Public key copied to clipboard!"
+    printf "${green}Public key copied to clipboard!"
 else
     if command -v xclip &> /dev/null; then
         xclip -sel clip < "$ssh_folder/$keyname.pub"
-	echo -e "${green}Public key copied to clipboard!"
+	printf "${green}Public key copied to clipboard!"
     elif command -v xsel &> /dev/null; then
         xsel --clipboard < "$ssh_folder/$keyname.pub"
-	echo -e "${green}Public key copied to clipboard!"
+	printf "${green}Public key copied to clipboard!"
     else
-        echo -e "${red}Neither xclip nor xsel found. Please install one of them."
+        printf "${red}Neither xclip nor xsel found. Please install one of them."
         exit 1
     fi
 fi
-echo -e "\nGo to https://github.com/settings/profile -> SSH and GPG Keys\nChoose New SSH key and paste the .pub key contents in your clipboard"
+printf "\nGo to https://github.com/settings/profile -> SSH and GPG Keys\nChoose New SSH key and paste the .pub key contents in your clipboard"
